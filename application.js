c1180e60c02f5b7b616288700da2051761ed4122
Eben Palazzolo <olozzalap@gmail.com>
2013-06-25 17:26:08 -0400

    application.js update and dom overhaul with different headlines

--- application.js
+++ application.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-	$(".app_links").on('mouseenter', function(){
-		$(this).closest("#headline").hide();
+	$("#basecamp").on('mouseenter', function(){
+		$(this).parent().siblings('#headline').children('.default_headline').hide();
+	});
+	$("#basecamp").on('mouseleave', function(){
+		$(this).parent().siblings('#headline').children('.default_headline').show();
+	});
+	$("#highrise").on('mouseenter', function(){
+		$(this).parent().siblings('#headline').children('.default_headline').hide();
+	});
+	$("#highrise").on('mouseleave', function(){
+		$(this).parent().siblings('#headline').children('.default_headline').show();
+	});
+	$("#campfire").on('mouseenter', function(){
+		$(this).parent().siblings('#headline').children('.default_headline').hide();
+	});
+	$("#campfire").on('mouseleave', function(){
+		$(this).parent().siblings('#headline').children('.default_headline').show();
 	});
 });
