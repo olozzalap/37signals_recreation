86aad4542d957a04fabbb1471036f8dd4a1113e3
Eben Palazzolo <olozzalap@gmail.com>
2013-06-26 19:21:19 -0400

    js and font update

--- application.js
+++ application.js
@@ -1,20 +1,36 @@
 $(document).ready(function(){
 	$("#basecamp").on('mouseenter', function(){
 		$(this).parent().siblings('#headline').children('.default_headline').hide();
+		$(this).parent().siblings('#headline').children('.basecamp_headline').show();
+	});
+	$(".basecamp_headline").on('mouseenter', function(){
+		$(this).siblings('.default_headline').hide();
+		$(this).show();
+	});
+	$(".basecamp_headline").on('mouseleave', function(){
+		$(this).siblings('.default_headline').show();
+		$(this).hide();
 	});
 	$("#basecamp").on('mouseleave', function(){
 		$(this).parent().siblings('#headline').children('.default_headline').show();
+		$(this).parent().siblings('#headline').children('.basecamp_headline').hide();
 	});
+
 	$("#highrise").on('mouseenter', function(){
 		$(this).parent().siblings('#headline').children('.default_headline').hide();
+		$(this).parent().siblings('#headline').children('.highrise_headline').show();
 	});
-	$("#highrise").on('mouseleave', function(){
+	$("#highrise,.highrise_headline").on('mouseleave', function(){
 		$(this).parent().siblings('#headline').children('.default_headline').show();
+		$(this).parent().siblings('#headline').children('.highrise_headline').hide();
 	});
+
 	$("#campfire").on('mouseenter', function(){
 		$(this).parent().siblings('#headline').children('.default_headline').hide();
+		$(this).parent().siblings('#headline').children('.campfire_headline').show();
 	});
-	$("#campfire").on('mouseleave', function(){
+	$("#campfire,.campfire_headline").on('mouseleave', function(){
 		$(this).parent().siblings('#headline').children('.default_headline').show();
+		$(this).parent().siblings('#headline').children('.campfire_headline').hide();
 	});
 });
